ea22427f03355817a2d899c073ea9ed1c6504186
Daran Dachte <darandachte@gmail.com>
2023-10-02 22:24:37 +0200

    wor with styles-4

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -1,11 +1,10 @@
 import Top from "./Top/Top";
 import Bottom from "./Bottom/Bottom";
 import AddedInformation from "./AddedInformation/AddedInformation";
-import bgImageDaytime from "../../assets/img/bgImageDaytime.jpg";
 
 const Container = () => {
   return (
-    <div className="w-screen h-screen bg-opacity-40 bg-cover bg-center bg-[url('src/assets/img/bgImageDaytime.jpg')]  ">
+    <div className="w-screen  h-screen bg-gradient-to-r from-black/[.8] to-black/[.8]  bg-cover bg-center bg-[url('src/assets/img/bgImageDaytime.jpg')] ">
       <Top />
       <Bottom />
       <AddedInformation />
